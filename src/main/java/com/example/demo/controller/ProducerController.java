a848eb65cfa54de9aee2d8b549085246b6373b28
rajnandini.ranbhare <rajnandini.ranbhare@joshsoftware.com>
2024-12-11 17:08:15 +0530

    added reflection config and build arguments

--- src/main/java/com/example/demo/controller/ProducerController.java
+++ src/main/java/com/example/demo/controller/ProducerController.java
@@ -3,18 +3,24 @@ package com.example.demo.controller;
 import com.example.demo.config.PubSubOptions;
 import com.example.demo.producer.DaprProducerNew;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+//import reactor.core.publisher.Mono;
 
 @RestController
+@RequestMapping("dapr")
 public class ProducerController {
 
     @Autowired
     private DaprProducerNew producerNew;
 
-    public Mono<Void> publishMessage(@RequestBody PubSubOptions message) {
+    @PostMapping("/publish")
+    public Mono<String> publishMessage(@RequestBody PubSubOptions message) {
 
-        return producerNew.publishEvent(message);
+        producerNew.publishEvent(message);
+        return Mono.just("success");
     }
 }
